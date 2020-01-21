2667dab0b42068e6a85cabf4e71521cd4ceac80e
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-21 02:00:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach = {},
-      :matriach = {},
-      :hero = {},
-      :hero_freinds = []
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
+      :hero_freinds => []
       }, 
     :capulet => {
-      :patriach = {},
-      :matriach = {},
-      :heorine = {},
-      :heroine_friends = []
+      :patriach => {},
+      :matriach => {},
+      :heorine => {},
+      :heroine_friends => []
   }
 
 
